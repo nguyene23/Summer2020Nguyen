c8d582a6fd075d33250ebfba9ddce73f2b604ab8
Emma Nguyen <nguyene23@greenhill.org>
2020-06-10 10:32:15 -0500

    New Stuff TM!

---
+++ src/coolguy/gangstaname.java
@@ -0,0 +1,18 @@
+package coolguy;
+import java.util.Scanner;
+public class gangstaname {
+    public static void main (String[] args){
+// to make a scanner console, give it a name -- need the new LINE
+        Scanner console = new Scanner(System.in);
+        System.out.print("Type your name, playa: ");
+        String name = console.nextLine(); //next only takes ONE WORD--so need nextLine
+// split name into first/last name + initials
+        String first = name.substring(0, name.indexOf (" "));
+        String last = name.substring(name.indexOf (" ") + 1);
+        last = last.toUpperCase();
+        String fInitial = first.substring(0, 1);
+//print out gangsta name output
+        System.out.println
+                ("Your gangsta name is \"" + fInitial + ". Diddy " + last + " " + first + "-izzle\"");
+    }
+}
