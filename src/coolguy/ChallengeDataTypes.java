fe08600585a3dda95b57a0d45cd6933290fbe3f1
Emma Nguyen <nguyene23@greenhill.org>
2020-06-16 19:59:31 -0500

    Woah

---
+++ src/coolguy/ChallengeDataTypes.java
@@ -0,0 +1,66 @@
+package coolguy;
+// Emma Nguyen
+// 6/16/20
+
+//create and declare scanner
+//        Open bracket for main args
+//        create and initialize/declare variables
+//        Int, double, string; ex. int integy1 = 9, double doubly1 = 3.2, String str1 = "Hornets"
+//        declare and initialize int i, double d, String s in program
+//using scanner
+//        ask for input prompts for user utilizing scanner
+//        print line prompts on new line
+//        Repeat for string, integer, and double values
+//        Enter in function for math operations
+//        print the outcomes of the mouth using a function
+//        Close brackets
+//math operations function
+//        Open up new bracket, initialize and declare
+//        > attach to scanner potentially
+//        Print sum of int variable on a new line (integy+i) using printf
+//        Print sum + double variable to a scale of one decimal place on a new line (\n)
+//        Concatenate input and string s
+//        print the result on a new line (\n)
+//        Print the sum of both integers on the first line
+//        Print the sum of both doubles (scaled to decimal place) on the second line
+//        Print the two concatenated strings on the third line
+
+import java.util.Scanner;
+
+public class ChallengeDataTypes {
+    public static void main(String[] args){
+        // declare and initialize variables
+        int integy1;
+        double doubly1;
+        String str1;
+
+        //declare other variables -- these are 'i', 'd', and 's'
+        int integy2 = 4;
+        double doubly2 = 4.0;
+        String str2 = "Greenhill";
+
+        //ask for user input
+        //print line prompts on new line
+        //Repeat for string, integer, and double values
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter in an integer: ");
+        integy1 = console.nextInt();
+        System.out.println("Enter in a double: ");
+        doubly1 = console.nextDouble();
+        System.out.println("Enter in a string: ");
+        str1 = console.nextLine();
+
+        //(functions don't work) here are the math calculations
+        int calc1=integy1+integy2;
+        double calc2=doubly1+doubly2;
+        String calc3=(str1 + " " + str2);
+
+        //print them out
+        System.out.printf("Integer answer: \n%10d", calc1);
+        System.out.println();
+        System.out.printf("Double answer: \n%10.1f", calc2);
+        System.out.println();
+        System.out.println("String answer: \n" + calc3);
+    }
+
+}
--- src/coolguy/kshape.java
+++ src/coolguy/kshape.java
@@ -1,71 +1,29 @@
 package coolguy;
-
+//Emma Nguyen
 public class kshape {
     public static final int SIZE=6; //declaring the global constant
+    public static final char letter = 65;
     public static void main (String[] args){
         topline();
         bottomline();
 
     }
     public static void topline() {
-        for (int line = 1; line <= SIZE - 1; line--) {
-            char letter = 65;
-            for (byte count = 0; count < SIZE; count++) {
-                System.out.print((char) (letter + count) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 1; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 2; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 3; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 4; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count = 0; count < SIZE - 5; count++) {
-                    System.out.print((char) (letter + count) + " ");
-                }
-                pause();
+        int line = 1;
+        for (line = 1; line <= SIZE; line++);{
+            for(byte count = 0; count<= SIZE-line; count++){
+                System.out.print((char) (letter+count)+ " ");
+            }
+            System.out.println();
         }
     }
+
     public static void bottomline(){
-        for (int line = 1; line <= SIZE; line++) {
-            char letter = 65;
-                for (byte count = 0; count < SIZE - 5; count++) {
-                    System.out.print((char) (letter + count) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 4; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 3; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 2; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count2 = 0; count2 < SIZE - 1; count2++) {
-                    System.out.print((char) (letter + count2) + " ");
-                }
-                pause();
-                for (byte count = 0; count < SIZE; count++) {
-                    System.out.print((char) (letter + count) + " ");
-                }
+        for (int line = SIZE; line >= 1; line--){
+            for (byte count = 0; count <= SIZE - line; count++){
+                System.out.print((char) (letter+count)+" ");
+            }
+            System.out.println();
         }
-    }
-    public static void pause(){
-        System.out.println();
     }
 }
-// what i have so far -- ask more in class tmw, confused on how to adjust size for this.
